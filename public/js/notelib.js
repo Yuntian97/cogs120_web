3669edbaf3498980a44a62d17f1fe6201d12dfc1
Yuntian Zheng <yuz451@ucsd.edu>
2018-03-13 23:17:37 -0700

    Combine View

--- public/js/notelib.js
+++ public/js/notelib.js
@@ -1,14 +1,34 @@
 var title;
-// Call this function when the page loads (the "ready" event)
-$(document).ready(function () {
-	initializePage();
-})
+var map;
+var marker;
+var flag = true;
+var Init;
+var Pos;
+var markerPos;
+var getPosObj;
+var markerArray = [];
+var infoArray = [];
+
+function initMap() {
+	Init = {
+		lat: 32.878,
+		lng: -117.234
+	};
+
+    console.log("This enter")
+	map = new google.maps.Map(document.getElementById('map'), {
+	    zoom: 13,
+	    center: Init
+	});
+    
+    callthis();
+};
 
 // Function that is called when the document is ready.
-function initializePage() {
+function callthis() {
 	console.log("Javascript connected!");
 	$('.thisName').click(nameClick);
-    
+    $.get("/getAllObject", getMOFn);
 }
 
 // what should we do when we press next
@@ -25,3 +45,28 @@ function nameClick(e) {
 function callbackFn(res) {
     window.location.href = "/mapViewS";
 }
+
+function getMOFn(res){
+    var oneLat;
+    var oneLng;
+    var oneTitle;
+    var onePos;
+    markerArray = [];
+    
+    res.notelib.forEach(function(element) {
+        oneLat = element.lat;
+        oneLng = element.lng;
+        oneTitle = element.title;
+        
+        onePos = new google.maps.LatLng(oneLat, oneLng);
+        new google.maps.Marker({
+            position: onePos,
+            map: map,
+            draggable: false});
+            
+        infoWindow = new google.maps.InfoWindow;
+        infoWindow.setPosition(onePos);
+        infoWindow.setContent(oneTitle);
+        infoWindow.open(map);
+    });
+};
